1e2e200b75d5662ac4fb1678c0c19106970a19f3
Pavel Dubov <dubov94@gmail.com>
2020-04-21 20:09:36 +0100

    Replace manual argv parsing with minimist

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const path = require('path')
+const minimist = require('minimist')
 
-const maybePopKwOption = (argv, optionName) => {
-  const optionPrefix = `--${optionName}=`
-  for (let index = 2; index < argv.length; ++index) {
-    const argument = argv[index]
-    if (argument.startsWith(optionPrefix)) {
-      argv.splice(index, 1)
-      return argument.slice(optionPrefix.length)
-    }
-  }
-  return null
+const PACKAGE_JSON_PATH_NAME = 'package-json-path'
+
+const args = minimist(process.argv.slice(2))
+
+if (args.hasOwnProperty(PACKAGE_JSON_PATH_NAME)) {
+  process.env['VUE_CLI_CONTEXT'] = path.dirname(
+      path.resolve(args[PACKAGE_JSON_PATH_NAME]))
 }
-
-const main = (argv, env) => {
-  let maybePackageJsonPath = maybePopKwOption(
-      argv, 'package-json-path')
-  if (maybePackageJsonPath !== null) {
-    env['VUE_CLI_CONTEXT'] = path.dirname(
-        path.resolve(maybePackageJsonPath))
-  }
-}
-
-main(process.argv, process.env)
 
 require('@vue/cli-service/bin/vue-cli-service.js')
